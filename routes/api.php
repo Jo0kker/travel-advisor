d4cda322daabb8268d3cdc9efbb8daf60b43efa2
Benjamin <boublou@hotmail.fr>
2023-12-14 11:09:28 +0100

    fix route reset password

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ use Lomkit\Rest\Facades\Rest;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
